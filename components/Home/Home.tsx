040362db47343a1811a3aff9c24f9f89df66e3f3
Miruuw <saidammir80@gmail.com>
2025-05-02 19:29:26 +0700

    service and projects done

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import About from './About/About'
+import Services from './Services/Services'
+import Project from './Project/Project'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
       <About />
+      <Services/>
+      <Project/>
       {/* <Navbar /> */}
       {/* <About /> */}
       {/* <Skills /> */}
