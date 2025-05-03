f3ac2193c40c7e1dfcf299477c579d1f7a0e1e84
Miruuw <saidammir80@gmail.com>
2025-05-03 18:52:02 +0700

    skill done

---
+++ components/Home/Reviews/Reviews.jsx
@@ -0,0 +1,15 @@
+import SectionHeading from '@/components/Helper/SectionHeading'
+import React from 'react'
+
+const Reviews = () => {
+  return (
+    <div className='pt-16 pb-16 bg-[#050709]'>
+      <SectionHeading>Client Reviews</SectionHeading>
+      <div className='w-[90%] sm:w-[80%] mx-auto mt-20'>
+        {/* slider */}
+      </div>
+    </div>
+  )
+}
+
+export default Reviews
---
+++ components/Home/Skills/Skills.tsx
@@ -0,0 +1,24 @@
+import SectionHeading from '@/components/Helper/SectionHeading'
+import { skillsData } from '@/Data/data'
+import React from 'react'
+import SkillCard from './SkillCard'
+
+const Skills = () => {
+  return (
+    <div className='pt-16 pb-16 bg-[#050709]'>
+    <SectionHeading>My Skills</SectionHeading>
+    <div className='mt-20 w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 items-center'>
+        {skillsData.map((skill)=> {
+            return (
+            <div>
+                <SkillCard skill={skill}/>
+            </div>
+            )
+            
+        })}
+    </div>
+    </div>
+  )
+}
+
+export default Skills
