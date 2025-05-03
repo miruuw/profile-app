900f817f21defd6342102b947c1992beccdfece8
Miruuw <saidammir80@gmail.com>
2025-05-03 18:51:50 +0700

    skill done

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -3,6 +3,8 @@ import Hero from './Hero/Hero'
 import About from './About/About'
 import Services from './Services/Services'
 import Project from './Project/Project'
+import Skills from './Skills/Skills'
+import Reviews from './Reviews/Reviews'
 
 const Home = () => {
   return (
@@ -11,12 +13,8 @@ const Home = () => {
       <About />
       <Services/>
       <Project/>
-      {/* <Navbar /> */}
-      {/* <About /> */}
-      {/* <Skills /> */}
-      {/* <Projects /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}  
+      <Skills/>
+      <Reviews/>  
     </div>
   )
 }
